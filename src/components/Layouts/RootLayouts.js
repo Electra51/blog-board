d9362668ad99c8994207a12a7109365cfbaab16e
safayet <safayet@merchantbay.com>
2023-07-27 17:37:47 +0600

    design update

--- src/components/Layouts/RootLayouts.js
+++ src/components/Layouts/RootLayouts.js
@@ -7,62 +7,50 @@ const RootLayouts = ({ children }) => {
     token: { colorBgContainer },
   } = theme.useToken();
   return (
-    <div>
-      {" "}
-      <Layout className="layout">
-        <Header
+    <Layout className="layout">
+      <Header
+        style={{
+          display: "flex",
+          alignItems: "center",
+        }}
+      >
+        <div className="demo-logo" />
+        <Menu
+          theme="dark"
+          mode="horizontal"
+          defaultSelectedKeys={["2"]}
+          items={new Array(15).fill(null).map((_, index) => {
+            const key = index + 1;
+            return {
+              key,
+              label: `nav ${key}`,
+            };
+          })}
+        />
+      </Header>
+      <Content
+        style={{
+          padding: "0 0px",
+        }}
+      >
+        <div
+          className="site-layout-content"
           style={{
-            display: "flex",
-            alignItems: "center",
+            background: colorBgContainer,
+            height: "100vh",
           }}
         >
-          <div className="demo-logo" />
-          <Menu
-            theme="dark"
-            mode="horizontal"
-            defaultSelectedKeys={["2"]}
-            items={new Array(15).fill(null).map((_, index) => {
-              const key = index + 1;
-              return {
-                key,
-                label: `nav ${key}`,
-              };
-            })}
-          />
-        </Header>
-        <Content
-          style={{
-            padding: "0 50px",
-          }}
-        >
-          <Breadcrumb
-            style={{
-              margin: "16px 0",
-            }}
-          >
-            <Breadcrumb.Item>Home</Breadcrumb.Item>
-            <Breadcrumb.Item>List</Breadcrumb.Item>
-            <Breadcrumb.Item>App</Breadcrumb.Item>
-          </Breadcrumb>
-          <div
-            className="site-layout-content"
-            style={{
-              background: colorBgContainer,
-              height: "100vh",
-            }}
-          >
-            {children}
-          </div>
-        </Content>
-        <Footer
-          style={{
-            textAlign: "center",
-          }}
-        >
-          Ant Design ©2023 Created by Ant UED
-        </Footer>
-      </Layout>
-    </div>
+          {children}
+        </div>
+      </Content>
+      <Footer
+        style={{
+          textAlign: "center",
+        }}
+      >
+        Ant Design ©2023 Created by Ant UED
+      </Footer>
+    </Layout>
   );
 };
 
