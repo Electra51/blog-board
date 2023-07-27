db2a15a0f1a1a23b01ff1a031e92aeeacab20e26
safayet <safayet@merchantbay.com>
2023-07-27 16:40:35 +0600

    initiallization

---
+++ src/pages/404.js
@@ -0,0 +1,12 @@
+import Image from "next/image";
+import React from "react";
+
+const ErrorPage = () => {
+  return (
+    <div>
+      <Image src="/images/eror.jpg" width={900} height={900}></Image>
+    </div>
+  );
+};
+
+export default ErrorPage;
